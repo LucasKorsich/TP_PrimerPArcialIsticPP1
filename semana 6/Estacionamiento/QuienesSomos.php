ed3d5b556432ca08ba9874decbd107a692a4c584
LucasKorsich <54085910+LucasKorsich@users.noreply.github.com>
2019-10-02 15:36:28 -0300

    Add files via upload

---
+++ semana 6/Estacionamiento/QuienesSomos.php
@@ -0,0 +1,93 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title>Sticky Footer Navbar Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="sticky-footer-navbar.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <header>
+      <!-- Fixed navbar -->
+      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <a class="navbar-brand" href="#">Estacionamiento L&C</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="Registro.php">Registrarse<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="Login.php">Ingresar</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="QuienesSomos.php">¿Quienes somos?</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+
+    <!-- Begin page content -->
+  <DIV>
+    <h1>Quienes Somos</h1>
+  </DIV>
+  <DIV>
+    <p>
+    <h3> 
+      Nuestro horarios y aranceles son:
+      Lunes a Viernes de 9 a 20 hs / Sábado de 9 a 14 hs.
+        
+          
+    </h3>
+    </p>
+  </DIV>
+    <div>
+    <h3>Nuestros aranceles son los siguientes,Puede consultar por Promosion:</h3>
+    
+    <p>
+    1/2 horas.....................................................................................................10$</p>
+    <p> 
+    1horas...................................................................................................... 30$</p>
+    <p>
+    2horas....................................................................................................... 100$</p>
+    <p>3 horas........................................................................................... 200$</p>
+    <p>4 horas................................................................................................. 500$</p>
+    <p>Mes Moto....................................................................................................... 1500$</p>
+    <p>Mes Auto....................................................................................................... 2500$</p>
+    <p>Mes Camionetas......................................................................................... 3500$</p>
+    </div>
+    <footer class="footer">
+      <div class="container">
+        <span class="text-muted">Place sticky footer content here.</span>
+      </div>
+    </footer>
+    <img src=" "height="900">
+    <footer class="footer">
+      <div class="container">
+        <span class="text-muted">Place sticky footer content here.</span>
+      </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src=" https://image.freepik.com/foto-gratis/desenfoque-imagen-bokeh-estacionamiento-coche-uso-fondo_7188-621.jpg" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="popper.min.js"></script>
+    <script src="popper.min.js"></script>
+  </body>
+</html>
